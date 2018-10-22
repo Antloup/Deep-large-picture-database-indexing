3dbc658dba550670f2921fd19c74fe2e72ed9a44
Benjamin Chazelle <benjamin.chazelle@outlook.fr>
2018-10-22 10:25:56 +0200

    Add test picture

---
+++ src/Training.py
@@ -0,0 +1,76 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import torch.nn as nn
+
+import torch.optim as optim
+
+from Net import Net
+
+
+
+plt.interactive(False)
+
+transform = transforms.Compose(
+    [transforms.Grayscale(),
+     transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+)
+
+
+def imshow(img):
+    img = img / 2 + 0.5
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+if __name__ == '__main__':
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    trainset = torchvision.datasets.ImageFolder(root="../train_images", transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=True, num_workers=1)
+
+    classes = ('Non-visage', 'Visage')
+
+    net = Net()
+    net.to(device)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+    for epoch in range(2):
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            inputs, labels = data
+            # imshow(torchvision.utils.make_grid(inputs))
+            # input("Press enter")
+            inputs, labels = inputs.to(device), labels.to(device)
+            optimizer.zero_grad()
+
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            if i % 200 == 199:
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 200))
+                running_loss = 0.0
+
+    print('Finished Training')
+
+    torch.save(net.state_dict(), "../model.pt")
+
+    dataiter = iter(trainloader)
+    features, _ = dataiter.next()
+    imshow(torchvision.utils.make_grid(features))
+    _, predicted = torch.max(net(features), 1)
+    print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
+                                  for j in range(4)))
+
